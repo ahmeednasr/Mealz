3c6d0602cfb50e2d0962a6f894601244fca7a06b
ahmeednasr <ahmedna7sr@gmail.com>
2023-10-17 19:42:34 +0300

    add app

--- app/src/main/java/com/example/mealzapp/MainActivity.kt
+++ app/src/main/java/com/example/mealzapp/MainActivity.kt
@@ -3,11 +3,27 @@ package com.example.mealzapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MealsViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel.getMeals()
+        val mealsAdapter = MealsAdapter()
+        val rv: RecyclerView = findViewById(R.id.category_rv)
+        lifecycleScope.launch {
+            viewModel.category.collect {
+                mealsAdapter.submitList(it?.categories)
+                rv.adapter = mealsAdapter
+            }
+        }
+
     }
+
 }
--- app/src/main/java/com/example/mealzapp/MealsApplication.kt
+++ app/src/main/java/com/example/mealzapp/MealsApplication.kt
@@ -4,4 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MealsApp : Application()
+class MealsApplication : Application()
